4f36e637924ae85dcdcf44ee6dece0d358ac463d
Marco Cacciatore <marco.cacciatore@students.unibe.ch>
2018-10-12 15:00:08 +0200

    fix in job frontend model

--- Project/ESE-2018-Scaffolding-master/frontend/src/app/job.ts
+++ Project/ESE-2018-Scaffolding-master/frontend/src/app/job.ts
@@ -7,7 +7,7 @@ export class Job {
     public name: string,
     public description: string,
     public company_name: string,
-    public wage: string,
+    public wage: number,
     public job_start: string,
     public job_end: string,
     public percentage: number,

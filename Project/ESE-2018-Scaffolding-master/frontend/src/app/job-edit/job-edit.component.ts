3e96365effc59276966ae21e39286130ac5a9d34
Marco Cacciatore <marco.cacciatore@students.unibe.ch>
2018-10-18 23:25:03 +0200

    implemented the approval

--- Project/ESE-2018-Scaffolding-master/frontend/src/app/job-edit/job-edit.component.ts
+++ Project/ESE-2018-Scaffolding-master/frontend/src/app/job-edit/job-edit.component.ts
@@ -2,6 +2,9 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Job} from '../job';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {JobService} from '../job.service';
+import {UserService} from '../user.service';
+import {Router} from '@angular/router';
+import {User} from '../user';
 
 @Component({
   selector: 'app-job-edit',
@@ -10,6 +13,7 @@ import {JobService} from '../job.service';
 })
 export class JobEditComponent implements OnInit {
   jobId: string;
+  user: User;
 
   @Input()
   job: Job;
@@ -20,12 +24,19 @@ export class JobEditComponent implements OnInit {
   @Output()
   destroy = new EventEmitter<Job>();
 
-  constructor() { }
+  constructor(public userService: UserService, public router: Router) { }
 
   ngOnInit() {
+    this.userService.currentUser.subscribe(currentUser => this.user = currentUser);
+    if(this.user === null || !this.user.isAuthenticated()){
+      this.router.navigateByUrl('/login');
+      return;
+    }
+    if(this.user.isModerator()){
+      this.editAsModerator = true;
+    }
     this.jobId = location.search.replace('?id=', '');
     if (location.search.search('id') === 1 && this.jobId.length >0){
-      console.log('found search: ' + this.jobId);
       JobService.getJobById(this.jobId).subscribe((instance: any) => {
         this.job = instance;
       });
@@ -42,6 +53,13 @@ export class JobEditComponent implements OnInit {
   onDestroy() {
     JobService.deleteJob(this.job).subscribe(() => {
       this.destroy.emit(this.job);
+    });
+  }
+
+  setPublic(){
+    this.job.approved = true;
+    JobService.saveJob(this.job).subscribe((instance: any) => {
+      this.job = instance;
     });
   }
 

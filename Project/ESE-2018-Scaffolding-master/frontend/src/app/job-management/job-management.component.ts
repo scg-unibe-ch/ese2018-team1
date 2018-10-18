a23dc569c59ff65f9895e908bdf70941d31389de
Marco Cacciatore <marco.cacciatore@students.unibe.ch>
2018-10-18 23:22:56 +0200

    integrated the profile into job-mngt

--- Project/ESE-2018-Scaffolding-master/frontend/src/app/job-management/job-management.component.ts
+++ Project/ESE-2018-Scaffolding-master/frontend/src/app/job-management/job-management.component.ts
@@ -4,6 +4,7 @@ import {HttpClient} from '@angular/common/http';
 import {User} from '../user';
 import {UserService} from '../user.service';
 import {JobService} from '../job.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-job-management',
@@ -14,14 +15,41 @@ export class JobManagementComponent implements OnInit {
   jobs: Job[] = [];
   user: User;
 
-  constructor(private httpClient: HttpClient, public userService: UserService) { }
+  oldPassword: string;
+  newPassword: string;
+  newPasswordRepeat: string;
+
+  showPassword = false;
+
+  constructor(private httpClient: HttpClient, public userService: UserService, public router: Router) {}
 
   ngOnInit() {
-    JobService.getAllJobs().subscribe((instances: any) => {
-      this.jobs = instances.map((instance) => new Job(instance.id, instance.name, instance.description, instance.company_name,
-        instance.wage, instance.job_start, instance.job_end, instance.percentage, instance.approved));
-    });
     this.userService.currentUser.subscribe(currentUser => this.user = currentUser);
+    if(this.user === null || !this.user.isAuthenticated()){
+      console.log('not auth');
+      this.router.navigateByUrl('/login');
+      return;
+    }
+    if(this.user.isCompany()) {
+      JobService.getJobsByCompany(this.user.name).subscribe((instances: any) => {
+        this.jobs = instances.map((instance) => new Job(instance.id, instance.name, instance.description_short, instance.description, instance.company_name,
+          instance.wage, instance.job_start, instance.job_end, instance.percentage, instance.approved));
+      });
+    }
+
+    if(this.user.isModerator()){
+      JobService.getAllJobs().subscribe((instances: any) => {
+        this.jobs = instances.map((instance) => new Job(instance.id, instance.name, instance.description_short, instance.description, instance.company_name,
+          instance.wage, instance.job_start, instance.job_end, instance.percentage, instance.approved));
+      });
+    }
   }
 
+  togglePasswordShow(){
+    this.showPassword = !this.showPassword;
+  }
+
+  savePassword(){
+    // TODO: call userService to change the password when userService has implemented it
+  }
 }

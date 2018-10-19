42a65ef85b7b8e2099b5b2b3485d81d77163bf57
Marco Cacciatore <marco.cacciatore@students.unibe.ch>
2018-10-19 15:42:28 +0200

    started with public company profile

--- Project/ESE-2018-Scaffolding-master/frontend/src/app/job-management/job-management.component.ts
+++ Project/ESE-2018-Scaffolding-master/frontend/src/app/job-management/job-management.component.ts
@@ -18,30 +18,49 @@ export class JobManagementComponent implements OnInit {
   oldPassword: string;
   newPassword: string;
   newPasswordRepeat: string;
+  companyId: string;
+  company: User;
+  public: boolean;
 
   showPassword = false;
 
   constructor(private httpClient: HttpClient, public userService: UserService, public router: Router) {}
 
   ngOnInit() {
-    this.userService.currentUser.subscribe(currentUser => this.user = currentUser);
-    if(this.user === null || !this.user.isAuthenticated()){
-      console.log('not auth');
-      this.router.navigateByUrl('/login');
-      return;
+    this.companyId = location.search.replace('?id=', '');
+    if (location.search.search('id') === 1 && this.companyId.length >0){
+      this.userService.getUseryId(this.companyId).subscribe((instance: any)=>{
+        this.user = instance;
+        JobService.getJobsByCompany(this.user.id, true).subscribe((instances: any) => {
+          this.jobs = instances.map((instance) => new Job(instance.id, instance.name, instance.description_short, instance.description, instance.company_id, instance.company_email, instance.job_website,
+            instance.wage, instance.wagePerHour, instance.job_start, instance.job_end, instance.percentage, instance.approved));
+        });
+      })
+      this.public  = true;
     }
-    if(this.user.isCompany()) {
-      JobService.getJobsByCompany(this.user.name).subscribe((instances: any) => {
-        this.jobs = instances.map((instance) => new Job(instance.id, instance.name, instance.description_short, instance.description, instance.company_name,
-          instance.wage, instance.job_start, instance.job_end, instance.percentage, instance.approved));
-      });
-    }
+    else {
+      this.public = false;
+      this.userService.currentUser.subscribe(currentUser => this.user = currentUser);
+      if(this.user === null || !this.user.isAuthenticated()){
+        console.log('not auth');
+        this.router.navigateByUrl('/login');
+        return;
+      }
+      if (this.user.isCompany()) {
+        console.log('auth as company');
+        JobService.getJobsByCompany(this.user.id, false).subscribe((instances: any) => {
+          this.jobs = instances.map((instance) => new Job(instance.id, instance.name, instance.description_short, instance.description, instance.company_id, instance.company_email, instance.job_website,
+            instance.wage, instance.wagePerHour, instance.job_start, instance.job_end, instance.percentage, instance.approved));
+          console.log('got jobs:' + this.jobs.length);
+        });
+      }
 
-    if(this.user.isModerator()){
-      JobService.getAllJobs().subscribe((instances: any) => {
-        this.jobs = instances.map((instance) => new Job(instance.id, instance.name, instance.description_short, instance.description, instance.company_name,
-          instance.wage, instance.job_start, instance.job_end, instance.percentage, instance.approved));
-      });
+      if (this.user.isModerator()) {
+        JobService.getAllJobs().subscribe((instances: any) => {
+          this.jobs = instances.map((instance) => new Job(instance.id, instance.name, instance.description_short, instance.description, instance.company_id, instance.company_email, instance.job_website,
+            instance.wage, instance.wagePerHour, instance.job_start, instance.job_end, instance.percentage, instance.approved));
+        });
+      }
     }
   }
 

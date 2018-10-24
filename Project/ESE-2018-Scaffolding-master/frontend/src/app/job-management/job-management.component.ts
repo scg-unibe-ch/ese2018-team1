bc4b0b75adf599527eca393054b7066e08b3194f
Marco Cacciatore <marco.cacciatore@students.unibe.ch>
2018-10-24 23:34:07 +0200

    cleaned job-mngt

--- Project/ESE-2018-Scaffolding-master/frontend/src/app/job-management/job-management.component.ts
+++ Project/ESE-2018-Scaffolding-master/frontend/src/app/job-management/job-management.component.ts
@@ -35,9 +35,6 @@ export class JobManagementComponent implements OnInit {
   createNewJob = false;
   showNewJobEdit = false;
 
-  backendUrl = 'http://localhost:3000';//only used for development purposes
-  successfulRegister: boolean = true;
-
   constructor(private httpClient: HttpClient, public userService: UserService, public router: Router) {}
 
   ngOnInit() {
@@ -56,17 +53,14 @@ export class JobManagementComponent implements OnInit {
       this.public = false;
       this.userService.currentUser.subscribe((instance) => this.user = new User(instance.id, instance.name, '','', instance.email, instance.role));
       if(this.user === null || !this.userService.currentLoginStatus){
-        console.log('not auth');
         this.router.navigateByUrl('/login');
         return;
       }
       this.passwordChangeUserId = this.user.id;
       if (this.user.isCompany()) {
-        console.log('auth as company');
         JobService.getJobsByCompany(this.user.id, false).subscribe((instances: any) => {
           this.jobs = instances.map((instance) => new Job(instance.id, instance.name, instance.description_short, instance.description, instance.company_id, instance.company_email, instance.job_website,
             instance.wage, instance.wagePerHour, instance.job_start, instance.job_end, instance.percentage, instance.approved));
-          console.log('got jobs:' + this.jobs.length);
         });
       }
 
@@ -160,75 +154,19 @@ export class JobManagementComponent implements OnInit {
     this.createNewJob = !this.createNewJob;
   }
 
+  /**
+   * creates a new job and shows the job edit component
+   */
   onSubmit() {
-    console.log('job name:' + this.job.name);
-    console.log('company id:' + this.user.id);
     if (this.job.name) {
-      console.log('name is set');
       this.job.company_id = this.user.id + '';
       this.job.company_email = this.user.email;
       this.job.approved = false;
-      console.log('job company id:' + this.job.company_id);
       JobService.createJob(this.job, this.user).subscribe((instance: any) => {
-        console.log('created job');
         this.job = new Job(instance.id, instance.name, instance.description_short, instance.description, instance.company_id, instance.company_email, instance.job_website, instance.wage, instance.wagePerHour, instance.job_start, instance.job_end, instance.percentage, instance.approved);;
         this.showNewJobEdit = true;
       });
     }
 
-/*    this.httpClient.get(this.backendUrl + '/job/' + this.job.id, {withCredentials: true}).subscribe(
-      (instance: any) => { // if it creates an error, it means, that the id is not in the database yet
-        this.successfulRegister = false;
-        this.job = new Job(null, '', '', '', '', '', '', null, false, '', '', null, false);
-      },
-      err => {
-        this.httpClient.post(JobService.backendUrl + '/job/', {
-          withCredentials: true,
-          'id': this.job.id,
-          'name': this.job.name,
-          'description_short': this.job.description_short,
-          'description': this.job.description,
-          'company_id': this.job.company_id,
-          'company_email': this.job.company_email,
-          'job_website': this.job.job_website,
-          'wage': this.job.wage,
-          'wagePerHour': this.job.wagePerHour,
-          'jobStart': this.job.job_start,
-          'jobEnd': this.job.job_end,
-          'percentage': this.job.percentage,
-          'approved': this.job.approved
-        }).subscribe((instance: any) =>{
-          this.job.id = instance.id;
-          this.httpClient.get(JobService.backendUrl + '/job/' + this.job.id, {withCredentials: true}).subscribe(
-            (instance: any) =>{
-              this.job = new Job(instance.id, instance.name, instance.description_short, instance.description, instance.company_id, instance.company_email, instance.job_website, instance.wage, instance.wagePerHour, instance.job_start, instance.job_end, instance.percentage, instance.approved);
-            }
-          )
-        })*/
-/*        this.user.salt = 'TestSalt';
-        this.user.password = UserService.hashPassword(this.user.password, this.user.salt);
-        this.httpClient.post(UserService.backendUrl + '/login/',  {
-          withCredentials: true,
-          'id': this.user.id,
-          'name': this.user.name,
-          'password': this.user.password,
-          'salt': this.user.salt,
-          'email': this.user.email,
-          'role': this.user.role
-        }).subscribe((instance: any) => {
-          this.user.id = instance.id;
-          this.httpClient.get(UserService.backendUrl + '/login/' + this.user.id + '/' + this.user.password, {withCredentials: true}).subscribe(
-            (instance: any) =>{
-              this.user = new User(instance.id, instance.name,instance.password,instance.salt,instance.email, instance.role);
-              this.userService.changeLoginStatus(true);
-              this.userService.changeUser(this.user);
-              this.router.navigate(['/']);
-            },
-            err =>{
-              this.userService.changeErrorStatus(true);
-            });
-        });*/
-//      });
-    //this.createNewJob = false;
   }
 }

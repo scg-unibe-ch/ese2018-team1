015de9548ddc74b34f193cb42d1d19f7da5d04a1
Marco Cacciatore <marco.cacciatore@students.unibe.ch>
2018-11-09 00:00:35 +0100

    Revert "tiny fix"
    This reverts commit 537d864de33b1942306a2741b3fc4d12c5f74383.

--- Project/ESE-2018-Scaffolding-master/frontend/src/app/Profil/profil-edit/profil-edit.component.ts
+++ Project/ESE-2018-Scaffolding-master/frontend/src/app/Profil/profil-edit/profil-edit.component.ts
@@ -12,7 +12,7 @@ export class ProfilEditComponent implements OnInit {
 
   user: User;
   userEdit: User;
-  successfulChange = true;
+  successfulChange: boolean = true;
 
   @Input()
   userId: number;

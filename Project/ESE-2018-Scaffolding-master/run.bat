480697220bf77e1f057f7dc048f9531caae0877e
Marco Cacciatore <marco.cacciatore@students.unibe.ch>
2018-11-09 10:08:18 +0100

    faster run batch

--- Project/ESE-2018-Scaffolding-master/run.bat
+++ Project/ESE-2018-Scaffolding-master/run.bat
@@ -1,7 +1,8 @@
+cd frontend
+start "Frontend" ng serve --open
+cd..
 cd backend
 start "Backend" npm run tsc 
 sleep 5
 start "Backend" node build/server.js
-cd..
-cd frontend
-start "Frontend" ng serve --open
+
